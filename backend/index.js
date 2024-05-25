cd0c8a69541782198fc7618595c10ec961d80703
krish808 <krishna143309@gmail.com>
2024-05-25 10:16:31 +0530

    backend Change

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,58 @@
 // Import required modules
+const port = 4000;
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
 // Initialize Express app
 const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
 
 // Middleware setup
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
+
+// Db Setup
+mongoose
+  .connect(
+    "mongodb+srv://ecommers:ecommers@cluster0.ltfehpc.mongodb.net/e-commerce"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
 // Sample route
 app.get("/", (req, res) => {
   res.send("Welcome to my backend server!");
 });
 
+// Image Storage Engine
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+//creating Upload Endpoint for Images
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, (error) => {
+  if (!error) {
+    console.log(`Server is running on port ${port}`);
+  } else {
+    console.log(`Error: ${error}`);
+  }
 });
